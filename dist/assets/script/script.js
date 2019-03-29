ddece133af63a5bb41b32caa712265ee501f646f
Heisenberg017 <suyash9211@gmail.com>
2019-03-29 16:48:46 +0530

    Faq Page Added And Minor Changes.

---
+++ dist/assets/script/script.js
@@ -0,0 +1,10 @@
+//FAQ
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion() {
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
