0c61bb101b9bae8c7d022c7deb8e89ff57b6b948
Heisenberg017 <suyash9211@gmail.com>
2020-04-14 20:29:08 +0530

    Working On ncertmitra

--- New Design/js/custom.js
+++ New Design/js/custom.js
@@ -4,7 +4,6 @@ jQuery(function ($) {
 	//Testimonial slide
 
 	$("#testimonial-slide").owlCarousel({
-
 		loop: false,
 		margin: 30,
 		nav: false,
@@ -12,19 +11,16 @@ jQuery(function ($) {
 		items: 3,
 		responsive: {
 			0: {
-				items: 1
+				items: 1,
 			},
 
-
 			600: {
-				items: 1
-			}
-		}
-
+				items: 1,
+			},
+		},
 	});
 
 	$(".performers-slide").owlCarousel({
-
 		loop: false,
 		margin: 10,
 		nav: false,
@@ -32,51 +28,74 @@ jQuery(function ($) {
 		items: 3,
 		responsive: {
 			0: {
-				items: 2
+				items: 2,
 			},
 
 			600: {
-				items: 2
+				items: 2,
 			},
 
 			900: {
-				items: 3
-			}
-		}
-
+				items: 3,
+			},
+		},
 	});
 
-	var btn = $('#back-to-top-btn');
+	var btn = $("#back-to-top-btn");
 
 	$(window).scroll(function () {
 		if ($(window).scrollTop() > 300) {
-			btn.addClass('show');
+			btn.addClass("show");
 		} else {
-			btn.removeClass('show');
+			btn.removeClass("show");
 		}
 	});
 
-	btn.on('click', function (e) {
+	btn.on("click", function (e) {
 		e.preventDefault();
-		$('html, body').animate({
-			scrollTop: 0
-		}, '300');
+		$("html, body").animate({
+				scrollTop: 0,
+			},
+			"300"
+		);
 	});
-
 });
 
-$('#play').on('click', function (e) {
+$("#play").on("click", function (e) {
 	e.preventDefault();
 	$("#player")[0].src += "?autoplay=1";
-	$('#player').show();
-	$('#video-cover').hide();
-	$('#play').hide();
-})
+	$("#player").show();
+	$("#video-cover").hide();
+	$("#play").hide();
+});
 
-$('#play-1').on('click', function (e) {
+$("#play-1").on("click", function (e) {
 	e.preventDefault();
 	$("#player-1")[0].src += "?autoplay=1";
-	$('#player-1').show();
-	$('#video-cover-1').hide();
-	$('#play-1').hide();
-})
+	$("#player-1").show();
+	$("#video-cover-1").hide();
+	$("#play-1").hide();
+});
+
+//Timer
+const second = 1000,
+	minute = second * 60,
+	hour = minute * 60,
+	day = hour * 24;
+
+let countDown = new Date("April 10, 2020 00:00:00").getTime(),
+	x = setInterval(function () {
+		let now = new Date().getTime(),
+			distance = countDown - now;
+
+		(document.getElementById("days").innerText = Math.floor(distance / day)),
+		(document.getElementById("hours").innerText = Math.floor(
+			(distance % day) / hour
+		)),
+		(document.getElementById("minutes").innerText = Math.floor(
+			(distance % hour) / minute
+		)),
+		(document.getElementById("seconds").innerText = Math.floor(
+			(distance % minute) / second
+		));
+	}, second);
